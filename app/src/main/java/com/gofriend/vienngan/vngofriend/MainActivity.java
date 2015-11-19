a530486490acdce15cf6418af50e82ad2690edd9
tmvien <minhvien93@gmail.com>
2015-11-19 16:50:10 +0700

    - Create SlideShow in screen main

--- app/src/main/java/com/gofriend/vienngan/vngofriend/MainActivity.java
+++ app/src/main/java/com/gofriend/vienngan/vngofriend/MainActivity.java
@@ -1,20 +1,32 @@
 package com.gofriend.vienngan.vngofriend;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.gofriend.vienngan.vngofriend.adapter.SlidingImageAdapter;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    SlidingImageAdapter mSlidingImageAdapter;
+    ViewPager mViewPager;
+    int currentPage = 0;
+    Timer swipeTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,10 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Start SlideShow
+        slideShow();
+        //End SlideShow
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +56,36 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    /**
+     * Show slide
+     */
+    private void slideShow() {
+        mSlidingImageAdapter = new SlidingImageAdapter(MainActivity.this);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSlidingImageAdapter);
+        final Handler handler = new Handler();
+
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == 6 - 1) {
+                    currentPage = 0;
+                }
+                mViewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 600, 2000);
+
     }
 
     @Override
@@ -98,4 +144,6 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
