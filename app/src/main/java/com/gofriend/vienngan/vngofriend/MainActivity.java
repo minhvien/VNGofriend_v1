166926f98e548b9b1deb74f653e503eee4afc5ae
tmvien <minhvien93@gmail.com>
2015-12-08 16:51:01 +0700

    - Config with parse

--- app/src/main/java/com/gofriend/vienngan/vngofriend/MainActivity.java
+++ app/src/main/java/com/gofriend/vienngan/vngofriend/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set tool bar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
